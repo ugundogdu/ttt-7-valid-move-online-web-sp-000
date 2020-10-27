b1c0489da385d30fb03e16582a14891d246e81c0
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-10-27 16:45:28 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
 def valid_move?(board, index)
-  if !position_taken?(board, index) && (index).between?(0,8)
+  if !position_taken?(board, index) && (index).between?(1,9)
     return true
   else
     return false
