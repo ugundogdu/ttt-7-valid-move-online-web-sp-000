ad1c6971a7ec849fab647e73a0b6a6017e9b499a
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-10-27 16:36:02 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,8 @@
-# code your #valid_move? method here
+def valid_move?
+  if player number = 1
+    puts number - 1
+  end 
+  # code your #valid_move? method here
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
