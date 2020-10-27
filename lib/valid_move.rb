fd088ab326cccf38f9057af60b2548f543bcc9d6
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-10-27 16:44:57 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,10 @@
-def valid_move?
-  
+def valid_move?(board, index)
+  if !position_taken?(board, index) && (index).between?(0,8)
+    return true
+  else
+    return false
   end
+end
   # code your #valid_move? method here
 
 
